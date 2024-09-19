5ae1e8f5823694e585f38f35035d0165d1ec3764
Ahmad Iqbal Firmansyah <ahmedeqbal101@gmail.com>
2024-09-19 11:00:32 +0700

    Pertemuan 3

--- pertemuan_3/bin/calculator.dart
+++ pertemuan_3/bin/calculator.dart
@@ -1,14 +1,14 @@
-import 'package:pertemuan_3/pertemuan_3.dart' as pertemuan_3;
 import 'dart:io';
 
 void main() {
-  int jmlSmt, jmlMk, jumlahSks = 0, totalSks = 0;
-  double ipk = 0.0, totalNr = 0.0;
-  List<int> sksSmt = [];
-  List<List<String>> matkul = [];
-  List<List<int>> sks = [];
-  List<List<String>> nilaiHuruf = [];
-  List<double> nr = [];
+  int i, j, nilai, jmlSmt, jmlMk = 0, jumlahNilai, jumlahSks, totalSks = 0;
+  List<List<int>> sks = List.generate(50, (i) => List.filled(30, 0));
+  List<int> sksSmt = List.filled(14, 0);
+  List<List<String>> nilaihuruf = List.generate(50, (_) => List.filled(30, ''));
+  List<List<String>> matkul = List.generate(50, (_) => List.filled(50, ''));
+  var mk;
+  List<double> nr = List.filled(14, 0);
+  double ipk, totalNr = 0;
 
   print("==============================================");
   print("\tProgram Menghitung IPK Mahasiswa");
@@ -20,93 +20,76 @@ void main() {
   if (jmlSmt < 2 || jmlSmt > 14) {
     print("Jumlah semester salah!");
     return;
-  }
+  } else {
+    for (i = 0; i < jmlSmt; i++) {
+      jumlahNilai = 0;
+      jumlahSks = 0;
+      stdout.write("Masukkan jumlah mata kuliah semester ${i + 1}: ");
+      jmlMk = int.parse(stdin.readLineSync()!);
+      if (jmlMk < 2) {
+        print("Jumlah matakuliah kurang dari 2 setiap semester");
+        return;
+      } else {
+        for (j = 0; j < jmlMk; j++) {
+          print("Masukkan mata kuliah ke ${j + 1}");
+          stdout.write("Masukkan nama matkul: ");
+          matkul[i][j] = stdin.readLineSync()!;
+          stdout.write("Masukkan jumlah sks matkul: ");
+          sks[i][j] = int.parse(stdin.readLineSync()!);
+          stdout.write("Masukkan nilai matkul(A/B/C/D): ");
+          nilaihuruf[i][j] = stdin.readLineSync()!;
+          print("--------------------------------------------");
 
-  for (int i = 0; i < jmlSmt; i++) {
-    int jumlahNilai = 0, jumlahSksSmt = 0;
-
-    stdout.write("Masukkan jumlah mata kuliah semester ${i + 1}: ");
-    jmlMk = int.parse(stdin.readLineSync()!);
-
-    if (jmlMk < 2) {
-      print("Jumlah mata kuliah kurang dari 2 setiap semester");
-      return;
-    }
-
-    List<String> mkPerSemester = [];
-    List<int> sksPerSemester = [];
-    List<String> nilaiHurufPerSemester = [];
-
-    for (int j = 0; j < jmlMk; j++) {
-      stdout.write("Masukkan mata kuliah ke-${j + 1}: ");
-      mkPerSemester.add(stdin.readLineSync()!);
-
-      stdout.write("Masukkan jumlah sks matkul: ");
-      sksPerSemester.add(int.parse(stdin.readLineSync()!));
-
-      stdout.write("Masukkan nilai matkul (A/B/C/D/E): ");
-      nilaiHurufPerSemester.add(stdin.readLineSync()!);
-
-      int nilai = 0;
-      switch (nilaiHurufPerSemester[j]) {
-        case 'A':
-          nilai = 4 * sksPerSemester[j];
-          break;
-        case 'B':
-          nilai = 3 * sksPerSemester[j];
-          break;
-        case 'C':
-          nilai = 2 * sksPerSemester[j];
-          break;
-        case 'D':
-          nilai = 1 * sksPerSemester[j];
-          break;
-        case 'E':
-          nilai = 0 * sksPerSemester[j];
-          break;
-        default:
-          print("Input salah!");
+          switch (nilaihuruf[i][j]) {
+            case 'A':
+              nilai = 4 * sks[i][j];
+              break;
+            case 'B':
+              nilai = 3 * sks[i][j];
+              break;
+            case 'C':
+              nilai = 2 * sks[i][j];
+              break;
+            case 'D':
+              nilai = 1 * sks[i][j];
+              break;
+            case 'E':
+              nilai = 0 * sks[i][j];
+              break;
+            default:
+              print("Input salah!");
+              return;
+          }
+          jumlahNilai += nilai;
+          jumlahSks += sks[i][j];
+        }
+        if (jumlahSks > 24) {
+          print("Jumlah SKS semester lebih dari 24");
           return;
+        } else {
+          sksSmt[i] = jumlahSks;
+          nr[i] = jumlahNilai / jumlahSks;
+        }
       }
-
-      jumlahNilai += nilai;
-      jumlahSksSmt += sksPerSemester[j];
     }
-
-    if (jumlahSksSmt > 24) {
-      print("Jumlah SKS semester lebih dari 24");
-      return;
-    } else {
-      sksSmt.add(jumlahSksSmt);
-      nr.add(jumlahNilai / jumlahSksSmt);
-    }
-
-    matkul.add(mkPerSemester);
-    sks.add(sksPerSemester);
-    nilaiHuruf.add(nilaiHurufPerSemester);
   }
 
   print("==============================================");
   print("\t\tTranskrip Nilai");
   print("==============================================");
-
-  for (int i = 0; i < jmlSmt; i++) {
-    print("\nHasil Semester ${i + 1}:\n");
-    print("Mata Kuliah\tSKS\tNilai");
-    for (int j = 0; j < matkul[i].length; j++) {
-      print("${matkul[i][j]}\t\t${sks[i][j]}\t${nilaiHuruf[i][j]}");
+  for (i = 0; i < jmlSmt; i++) {
+    print("\nHasil Semester ${i + 1}:");
+    print("\n${"Mata Kuliah".padRight(12)}${"SKS".padRight(12)}${"Nilai".padRight(12)}");
+    for (j = 0; j < jmlMk; j++) {
+      print("\n${matkul[i][j].padRight(12)}${sks[i][j].toString().padRight(12)}${nilaihuruf[i][j].padRight(12)}");
     }
-
-    totalSks += sksSmt[i];
-    totalNr += nr[i];
-
     print("\nSKS\t: ${sksSmt[i]}");
     print("NR\t: ${nr[i].toStringAsFixed(2)}");
+    totalSks += sksSmt[i];
+    totalNr += nr[i];
     print("--------------------------------------------");
   }
-
   ipk = totalNr / jmlSmt;
-
   print("\nTotal SKS\t: $totalSks");
   print("IPK\t\t: ${ipk.toStringAsFixed(2)}");
   print("==============================================");
--- pertemuan_3/bin/praktikum_1.dart
+++ pertemuan_3/bin/praktikum_1.dart
@@ -1,4 +1,3 @@
-import 'package:pertemuan_3/pertemuan_3.dart' as pertemuan_3;
 
 void main() {
   String test = "test2";
